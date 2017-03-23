cd791c77c716c9ac9203248852fa1204c7bfe0e6
shahnazshariff <shahnaz@entrayn.com>
2017-03-23 22:26:26 +0530

    beautify UI

--- trump_web/crawl_web.py
+++ trump_web/crawl_web.py
@@ -38,30 +38,3 @@ def crawl_twitter(url):
         return(all_tweets[0:25])
     else:
         return(all_tweets)
-
-    # link_list = []
-    # result = requests.get(url)
-    # soup = BeautifulSoup(result.content,"html.parser")
-    # for h3tag in soup.find_all("h3",re.compile("cd__headline")):
-    #     for atag in h3tag.find_all(re.compile("^a")):
-    #         link_list.append(atag)
-    # return link_list
-
-# for tag in soup.find_all("div", re.compile("ksoContent")):
-#     for tag1 in tag.find_all(re.compile("^p")):
-
-        # # print tag1.text
-        # tag1 = tag1.text.decode('unicode-escape');
-        # # print tag1.string
-        # print
-        # tag1
-        # lang = guess_language.guessLanguageName(tag1)
-        # print
-        # lang
-        # if lang == 'English' or lang == 'Hindi' or lang == 'Nepali' or lang == 'Tamil' or lang == 'Telugu' or lang == 'Punjabi' or lang == 'Malayalam' or lang == 'Marathi' or lang == 'Gujarati' or lang == 'Bengali':
-        #     ans = "known"
-        # else:
-        #     ans = "unknown"
-        # with open("post_shahnaz.json", "a") as outfile:
-        #     json.dump({'post': tag1, 'language': lang, 'type': ans}, outfile, sort_keys=False)
-        #     outfile.write('\n')
