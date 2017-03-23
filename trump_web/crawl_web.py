9389bb4fc8ff9c7f35c0359d2faeacd9c21f105f
shahnazshariff <shahnaz@entrayn.com>
2017-03-23 18:36:28 +0530

    add ui elements and twitter crawler

--- trump_web/crawl_web.py
+++ trump_web/crawl_web.py
@@ -12,17 +12,32 @@ def crawl_cnn(url):
     soup = BeautifulSoup(result.content, 'xml')
 
     all_articles = []
-    new_article = {}
     for url in soup.find_all("url"):
             if "Trump" in url.title.string:
                 new_article = {"title": url.title.string,"link":url.loc.string,"img_url":url.image.string}
-                # new_article["title"] = url.title.string
-                # new_article["link"] = url.loc.string
-                # new_article["img_url"] = url.image.string
-                # all_articles.append(new_article)
+                all_articles.append(new_article)
     return all_articles[0:25]
 
-# def crawl_twitter(url):
+def crawl_twitter(url):
+    access_token = "AAAAAAAAAAAAAAAAAAAAAJsVzwAAAAAAxBRtCkAUnQPWnjXqhSwDtxDH5I8%3DXIqX8EZwvJoLO5sbjwsecrLeblk65NNGrG9H3WAMReF5WyXk4f"
+    get_headers = {"Authorization": 'Bearer '+access_token}
+    result = requests.get(url,headers=get_headers)
+    json_result = result.json()
+    all_tweets = []
+    for i in range(len(json_result["statuses"])):
+        # check if tweet was by Donald Trump
+        if len(json_result["statuses"][i]["entities"]["user_mentions"]) is 0:
+            try:
+                #Extract link within tweet
+                link = re.search("(?P<url>https?://[^\s]+)", json_result["statuses"][i]["text"]).group("url")
+            except:
+                link = None
+            new_tweet = {"text": json_result["statuses"][i]["text"],"link":link}
+            all_tweets.append(new_tweet)
+    if len(all_tweets) > 25:
+        return(all_tweets[0:25])
+    else:
+        return(all_tweets)
 
     # link_list = []
     # result = requests.get(url)
--- trump_web/views.py
+++ trump_web/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from trump_web.crawl_web import crawl_cnn
+from trump_web.crawl_web import crawl_cnn,crawl_twitter
 # Create your views here.
 
 
 def web_contents_view(request):
     cnn_contents = crawl_cnn('http://edition.cnn.com/sitemaps/sitemap-news.xml')
-    # return render(request,'index.html',{'news':cnn_contents})
-    return HttpResponse(cnn_contents,content_type = "application/json")
+    twitter_contents = crawl_twitter('https://api.twitter.com/1.1/search/tweets.json?q=from%3ArealDonaldTrump&count=75')
+    return render(request,'index.html',{'news':cnn_contents,'tweets':twitter_contents})
+    # return HttpResponse(cnn_contents,content_type = "application/json")
